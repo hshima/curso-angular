da10d6157add9979b425d9f43d6c6708ffc161da
hshimada <shimada.henrique@gmail.com>
2020-07-05 18:58:03 -0300

    adding infinite Scroll

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
 
@@ -15,8 +15,11 @@ export class FilmesService {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(): Observable<Filme[]>{
-    return this.http.get<Filme[]>(url);
+  listar(pagina: number, qtdPagina: number): Observable<Filme[]>{
+    var httpParams = new HttpParams();
+    httpParams = httpParams.set('_page', pagina.toString()); // Sets which page shall be searched
+    httpParams = httpParams.set('_limit', qtdPagina.toString()); // Sets which page shall be searched
+    return this.http.get<Filme[]>(url, {params: httpParams});
 
   }
 
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -9,12 +9,31 @@ import { Filme } from 'src/app/shared/models/filme';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  filmes: Filme[];
+  readonly qtdPagina = 4;
+  filmes: Filme[] = []; // Starts the variable a empty array, not a null (undefined) array
+  pagina = 0;
+
   constructor(private filmesService: FilmesService) { }
 
-  ngOnInit() {
-    this.filmesService.listar().subscribe((filmes: Filme[]) => this.filmes = filmes);
+  ngOnInit(): void {
+    //this.filmesService.listar().subscribe((filmes: Filme[]) => this.filmes = filmes); // retrieves all of the register
+    console.log("chamado metodo: ngOnInit");
+    this.listarFilmes();
+    this.onScroll();
   }
 
+  onScroll(): void {
+    console.log("chamado metodo: onScroll ");
+    this.listarFilmes();
+  }
+
+  private listarFilmes(): void {
+    this.pagina++;
+    this.filmesService.listar(this.pagina, this.qtdPagina)
+      .subscribe((filmes: Filme[]) => {
+        this.filmes.push( // makes a push to the existing result, instead of overwritting the actual content
+          ...filmes) // Adding tha spread operator allows the array to be appended
+        });
+  }
 
 }
