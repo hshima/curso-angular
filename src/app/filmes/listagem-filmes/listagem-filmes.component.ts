559023a275a169b057bd36939029e712402c8aba
hshimada <shimada.henrique@gmail.com>
2020-07-05 23:59:22 -0300

    Adding filter form

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+
+
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
+
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -12,18 +17,27 @@ export class ListagemFilmesComponent implements OnInit {
   readonly qtdPagina = 4;
   filmes: Filme[] = []; // Starts the variable a empty array, not a null (undefined) array
   pagina = 0;
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(
+    private filmesService: FilmesService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-    //this.filmesService.listar().subscribe((filmes: Filme[]) => this.filmes = filmes); // retrieves all of the register
-    console.log("chamado metodo: ngOnInit");
+    this.filtrosListagem = this.fb.group({
+      texto: [''], // Receives an empty array, as fields will be retrieved from DB
+      generos: ['']
+    });
+
+    this.generos = ['Ação', 'Aventura', 'Ficção Científica', 'Romance', 'Terror', 'comedia', 'drama'];
+
     this.listarFilmes();
     this.onScroll();
   }
 
   onScroll(): void {
-    console.log("chamado metodo: onScroll ");
     this.listarFilmes();
   }
 
